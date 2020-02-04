2a18878507863ae112c93a1b61b8243b476c626c
Rotimi Kirya <okirya@ucsd.edu>
2020-02-04 15:10:29 -0800

    Almost finished

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
 exports.addFriend = function(request, response) {    
-	// Your code goes here
+	var name = request.query.name;
+	var description = request.query.description;
+	var newFriend = {
+		"name" : name,
+		"description" : description,
+		"imageURL" : "https://lorempixel.com/400/400.people"
+	}
+	data.friends.push(newFriend);
  }
